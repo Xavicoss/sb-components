97a0456bce0b4cbc6292b918972e6c56407f8385
Xavicoss <xavicoss@gmail.com>
2022-06-07 11:43:53 -0500

    Primer Commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,43 @@
+import { formatDiagnosticsWithColorAndContext } from 'typescript';
+import './mylabel.css';
+
+export interface MyLabelProps {
+  /**
+   * Este es el mensaje a mostrar en la etiqueta
+  */
+    label: string;
+  /**
+   * Este es el tamano a mostrar en la etiqueta
+  */
+    size?: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * Este es la capitalizacion de la etiqueta
+  */
+    allCaps?: boolean;
+  /**
+   * Este es el color de la etiqueta
+  */
+    color?: 'primary' | 'secondary' | 'tertiary';
+  /**
+   * Color personalizado de la fuente
+  */
+    fontColor?: string;
+
+}
+
+export const MyLabel = ( { 
+    label = 'No Label', 
+    size = 'normal',
+    allCaps = false,
+    color = 'primary',
+    fontColor
+}: MyLabelProps ) => {
+  return (
+    <span 
+      className={ `label ${ size } text-${color}`}
+      style={{ color: fontColor } 
+      }>
+        { allCaps ? label.toUpperCase() : label }
+    </span>
+  )
+}
